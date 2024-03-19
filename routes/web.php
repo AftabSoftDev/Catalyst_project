8ada2e0fd466eaf49c4278dab7b9cb08556cd07f
AftabSoftDev <almakhdoom078692@gmail.com>
2024-03-19 09:09:31 +0530

    common controller create and route updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/dashboard', [CommonController::class, 'getUserData'])->name('dashboard');
+//     Route::view('/file-upload', 'fileUpload')->name('file');
+//     Route::post('file-uploading', [CommonController::class, 'fileUpload'])->name('file-uploads');
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +31,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
